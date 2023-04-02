16ce72593d2d11bc65433d46ee3f09d82a1bd375
s01k1m <sk618dev@gmail.com>
2023-04-03 00:31:18 +0900

    230403

--- 이것이코딩테스트다_파이썬/BFSDFS/미로.py
+++ 이것이코딩테스트다_파이썬/BFSDFS/미로.py
@@ -15,23 +15,25 @@ cnt = 0
 def bfs(arr, sx, sy, visited):
     global cnt
     queue = deque()
-    queue.append([sx,sy])
+    queue.append([sx, sy])
     visited[sx][sy] = 1
     while queue:
         x, y = queue.popleft()
         for dx, dy in [[1, 0], [-1, 0], [0, -1], [0, 1]]:
             nx = x + dx
             ny = y + dy
-            if 0 <= nx < N and 0 <= ny < M and visited[nx][ny] == 0 and arr[nx][ny] == 0:
+            if 0 <= nx < N and 0 <= ny < M and visited[nx][ny] == 0 and arr[nx][ny] == 1:
                 queue.append([nx, ny])
+                arr[nx][ny] = arr[x][y] + 1
                 visited[nx][ny] = 1
 
-    cnt += 1
+
 
 
 for i in range(N):
     for j in range(M):
-        if arr[i][j] == 0 and visited[i][j] == 0:
+        if arr[i][j] == 1 and visited[i][j] == 0:
             bfs(arr, i, j, visited)
 
-print(cnt)
+
+print(arr[N-1][M-1])
