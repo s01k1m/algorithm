47c87444c4c996d3255b355ccd95c02834845a35
s01k1m <sk618dev@gmail.com>
2023-04-17 11:40:11 +0900

    2304016

--- BOJ/7576_2.py
+++ BOJ/7576_2.py
@@ -1,8 +1,39 @@
-import sys
-sys.stdin=open("input.txt")
+from collections import deque
 
-
-
+# M = column
+# N = row
 M, N = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
 
+
+def find_tomatoes(arr, queue):
+    # 토마토(1)을 찾는 함수
+    for i in range(N):
+        for j in range(M):
+            if arr[i][j] == 1:
+                queue.append((0, i, j))
+
+
+queue = deque()
+find_tomatoes(arr, queue)  # 이제 큐에는 day0의 토마토의 좌표가 들어간다.
+visited = [[0] * M for _ in range(N)]  # 토마토의 좌파 방문 여부 체크 할 비지티드
+# 델타탐색하면서 이미 익은 토마토를 또 중복처리하는 것을 막기 위해서
+
+day = 0
+while queue:
+    cnt, x, y = queue.popleft()  # 퇌토의 좌표를 불러왔음
+    day = max(day, cnt)
+    for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:  # 십자방향 토마토를 익게 한다.
+        nx = x + dx
+        ny = y + dy
+
+        if 0 <= nx < N and 0 <= ny < M and arr[nx][ny] == 0:
+            arr[nx][ny] = 1
+
+            queue.append([cnt + 1, nx, ny])
+
+# day를 어디서 +1 하냐.ㅜㅜ
+if [1 for r in range(N) for c in range(M) if arr[r][c] == 0]:
+    day = -1
+
+print(day)
--- SWEA/4615_othello_game.py
+++ SWEA/4615_othello_game.py
@@ -6,7 +6,16 @@ T= int(input())
 
 # N : 한 변의 길이
 # M : 플레이어가 돌을 놓는 횟수
-N, M = map(int, input().split())
 
 for tc in range(1, T+1):
-    for
+    N, M = map(int, input().split())
+    # 열 행, 순서로 입력되고 1부터 인덱스 시작, 컬러(0:블랙 1:화이트)
+    y, x, c = map(int, input().split())
+    # 돌을 놓으면 위 와래 대각선같은색 돌이 있는지 탐색하고 그 사이 있는 돌을 모두 같은색으로
+
+
+
+
+    print(f'#{tc} {cnt_b} {cnt_w}')
+
+
