19fbbe1389c8b23201b70e02ff9167127ab5b3ed
s01k1m <sk618dev@gmail.com>
2023-06-01 23:58:46 +0900

    14215 재풀이

--- BOJ/14215_세막대.py
+++ BOJ/14215_세막대.py
@@ -1,16 +1,27 @@
-# 두개 이상의 막대의 길이가 같을 때, 정삼각형
-# 막대 길이가 다 다를때, (작은 2개의 막대 길이 합의 -1) + 작은 막대 2개
 
+
+'''
+정답은 두 가지의 경우로 나뉘게 됩니다.
+
+   2-1. c > a + b인 경우 막대기를 줄일 필요없으므로 최대 둘레의 길이가 a + b + c가 됩니다.
+
+   2-2. 그 외의 경우 막대기 c를 적절히 줄여야 합니다. c를 a + b - 1로 줄이면 됩니다.
+
+   따라서 답은 (a + b) * 2 - 1입니다.
+'''
 a, b, c = map(int, input().split())
-print('$$')
-arr = set([a, b, c])
+
+nums = [a,b,c]
+nums.sort()
+arr = set(nums)
 sorted(arr)
 
-if len(arr) == 2:
-    result = arr[0]*3
-
+if nums[2] < nums[0] + nums[1] :
+    result = 0
+    for i in range(3):
+        result += nums[i]
     print(result)
-elif len(arr) >= 3:
-    result = arr[0] * 2 + arr[1]*2 - 1
+else:
+    result = ( nums[0] + nums[1] ) * 2 - 1
     print(result)
 
