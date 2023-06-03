4a4a6612c2e76056303b3ea47815f5ca6f5b0151
s01k1m <sk618dev@gmail.com>
2023-06-03 19:04:55 +0900

    24313 재풀이

--- BOJ/24313.py
+++ BOJ/24313.py
@@ -1,13 +1,5 @@
-a1, a0 = map(int, input().split())
-c = int(input())
-n0 = int(input())
-
-result = True
-for n in range(0, c+1):
-    if a1 * n + a0 > c * n:
-        result = False
-
-if result == False:
-    print(0)
-else:
-    print(1)
+a,b=map(int,input().split())
+c=int(input())
+n=int(input())
+r=1 if a*n+b<=c*n and c>=a else 0
+print(r)
